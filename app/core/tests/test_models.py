e6bb26c1905064d70767d196e6c9d04f8c17ba39
Victor Cordova <victor.cordova@travelperk.com>
2020-05-13 17:56:24 +0200

    Added list ingredients feature

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -45,3 +45,10 @@ class ModelTests(TestCase):
         )
 
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        ingredient = models.Ingredient.objects.create(
+            user=get_sample_user(),
+            name='Cucumber'
+        )
+        self.assertEqual(str(ingredient), ingredient.name)
