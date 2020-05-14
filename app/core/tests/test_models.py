d483baf777980ebd212475cbfbcb3d1c1349583a
Victor Cordova <victor.cordova@travelperk.com>
2020-05-14 09:57:57 +0200

    Added recipe model

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -52,3 +52,14 @@ class ModelTests(TestCase):
             name='Cucumber'
         )
         self.assertEqual(str(ingredient), ingredient.name)
+
+
+    def test_recipe_str(self):
+        recipe = models.Recipe.objects.create(
+            user=get_sample_user(),
+            title='Steak and mushroom sauce',
+            time_minutes=5,
+            price=5.00
+        )
+
+        self.assertEqual(str(recipe), recipe.title)
